0f0085d5a9d51efbee974719d3825ce321c896b5
Igor Bruneli <igorbrunelimoreira@gmail.com>
2017-06-03 00:00:40 +0100

    Added drupal instalation files

---
+++ web/sites/settings.allsites.php
@@ -0,0 +1,26 @@
+<?php
+
+// Trusted Host Settings
+$settings['trusted_host_patterns'] = array(
+  '^localhost$',
+);
+
+// $site_path is made available by Drupal core.
+$sites_subdir = basename($site_path);
+
+$databases['default']['default'] = array (
+    'database' => 'sharewithme_main_' . $sites_subdir,
+    'username' => 'root',
+    'password' => 'root',
+    'host' => '127.0.0.1',
+    'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+    'driver' => 'mysql',
+);
+
+$config_directories[CONFIG_SYNC_DIRECTORY] = '../config/'. $sites_subdir . '/sync';
+
+$settings['install_profile'] = 'minimal';
+
+if (file_exists(__DIR__ . '/settings.allsites.local.php')) {
+ include __DIR__ . '/settings.allsites.local.php';
+}
